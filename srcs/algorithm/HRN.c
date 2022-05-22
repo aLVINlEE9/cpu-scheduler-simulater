07b1d8d6f2b99767e19eddcf6002fd063b908a24
aLVINlEE9 <fldh3746@gmail.com>
2022-05-22 16:36:20 +0900

    fix hrn

--- srcs/algorithm/HRN.c
+++ srcs/algorithm/HRN.c
@@ -6,19 +6,19 @@ void	*response_rate_moniter(void *pcb_v)
 	t_PCB	*pcb, *pcb_rec;
 	t_process_table_node	*process_table_node;
 	long double	response_rate, temp;
-	int	i, flag;
+	int	i, flag_1, flag_2;
 
 	pcb = (t_PCB *)pcb_v;
 	while (1)
 	{
 		sem_wait(pcb->data->moniter_wait);
-		// printf("ter %d l_ter%d\n", pcb->data->terminated, pcb->data->last_terminated);
+		// printf("ter %d l_ter%d state :%d id:%d\n", pcb->data->terminated, pcb->data->last_terminated, pcb->state, pcb->user_id);
 		if (pcb->state == WAITING && pcb->data->terminated != pcb->data->last_terminated)
 		{
 			printf("ter %d l_ter%d\n", pcb->data->terminated, pcb->data->last_terminated);
-			pcb->data->last_terminated = pcb->data->terminated;
 			i = -1;
-			flag = 0;
+			flag_1 = 0;
+			flag_2 = 0;
 			response_rate = 0;
 			temp = 1;
 			process_table_node = pcb->data->process_table->head->next;
@@ -27,23 +27,25 @@ void	*response_rate_moniter(void *pcb_v)
 				if (process_table_node->pcb->state != WAITING)
 				{
 					process_table_node = process_table_node->next;
+					flag_2++;
 					continue ;
 				}
 				else
 				{
-					response_rate = (((long double)get_time() - (long double)process_table_node->pcb->readyque_arrived_time) \
-									+ (long double)process_table_node->pcb->burst_time) / (long double)process_table_node->pcb->burst_time;
+					printf("id%d state%d\n", process_table_node->pcb->user_id, process_table_node->pcb->state);
+					pcb->data->last_terminated = pcb->data->terminated;
+					response_rate = (((long double)get_time() - (long double)process_table_node->pcb->readyque_arrived_time) + (long double)process_table_node->pcb->burst_time) / (long double)process_table_node->pcb->burst_time;
 					printf("%Lf\n", response_rate);
+					pcb_rec = process_table_node->pcb;
 					if (response_rate > temp)
 					{
 						temp = response_rate;
-						pcb_rec = process_table_node->pcb;
-						flag = 1;
+						flag_1 = 1;
 					}
 				}
 				process_table_node = process_table_node->next;
 			}
-			if (flag)
+			if (flag_1 || flag_2 == pcb->data->process_cores - 1)
 			{
 				printf("moniter :%d temp: %Lf\n", pcb_rec->user_id, temp);
 				pcb->data->done = pcb_rec->user_id;
@@ -54,6 +56,41 @@ void	*response_rate_moniter(void *pcb_v)
 			printf("moniter :%d\n", pcb->user_id);
 			pcb->data->done = pcb->user_id;
 		}
+		// else if (pcb->state == WAITING && pcb->data->terminated == pcb->data->last_terminated)
+		// {
+		// 	i = -1;
+		// 	flag = 0;
+		// 	process_table_node = pcb->data->process_table->head->next;
+		// 	while (++i < pcb->data->process_cores)
+		// 	{
+		// 		if (process_table_node->pcb->state != RUNNING)
+		// 			flag++;
+		// 		// printf(" %d %d\n", pcb->user_id, process_table_node->pcb->state);
+		// 		process_table_node = process_table_node->next;
+		// 	}
+		// 	if (flag == pcb->data->process_cores)
+		// 	{
+		// 		printf("moniter :%d\n", pcb->user_id);
+		// 		pcb->data->terminated = -1;
+		// 	}
+		// }
+		// else if (pcb->state == WAITING && pcb->data->terminated == pcb->data->last_terminated)
+		// {
+		// 	i = -1;
+		// 	flag = 0;
+		// 	process_table_node = pcb->data->process_table->head->next;
+		// 	while (++i < pcb->data->process_cores)
+		// 	{
+		// 		if (process_table_node->pcb->state != RUNNING)
+		// 			flag++;
+		// 		process_table_node = process_table_node->next;
+		// 	}
+		// 	if (flag == pcb->data->process_cores)
+		// 	{
+		// 		printf("%d\n", pcb->user_id);
+		// 		pcb->data->terminated = -1;
+		// 	}
+		// }
 		sem_post(pcb->data->moniter_wait);
 	}
 }
@@ -61,7 +98,7 @@ void	*response_rate_moniter(void *pcb_v)
 void	HRN_wait(t_data *data, t_PCB *pcb, int id)
 {
 	pcb->state = WAITING;
-	printf("wait in%d\n", id);
+	printf("wait in%d state:%d\n", id, pcb->state);
 	while (1)
 	{
 		sem_wait(data->stop);
